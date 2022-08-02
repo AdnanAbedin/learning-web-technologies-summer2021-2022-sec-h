305d99cedd66013dad7830a8de5ad980a24c8c66
AdnanAbedin <91457614+AdnanAbedin@users.noreply.github.com>
2022-08-02 13:56:09 +0600

    Add files via upload

---
+++ Final lab 4/home/home.php
@@ -0,0 +1,34 @@
+<?php
+if(isset($_COOKIE['status'])){
+session_start();
+?>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+<h1>Welcome Home, <?php echo $_SESSION['username']; ?></h1>
+
+<h3>Major: 
+<?php 
+if(empty($_SESSION['major'])){ echo "Not Declared"; }
+else{ echo $_SESSION['major']; }
+?>
+</h3>
+
+<a href="../home/not_available.php">Users</a><br>
+<a href="../home/not_available.php">Change Password</a><br>
+<a href="../home/not_available.php">Course Content</a><br>
+<a href="../home/not_available.php">Result</a><br>
+<a href="../home/not_available.php">Major Declaration</a><br>
+<a href="../home/not_available.php">Facilities</a><br>
+<a href="../home/not_available.php">Form</a><br>
+<a href="../home/not_available.php">Library</a><br>
+<a href="../home/not_available.php">Logout</a>
+</body>
+</html>
+<?php
+}else{
+echo "Login again!";
+}
+?>
